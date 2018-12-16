5bdd0fe73e5640cae5c40a82a7585b0486c6a83c
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-16 14:34:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,11 @@ class InteractiveRecord
   end
   
   def self.find_by_name
-  
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE name = #{name}
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def self.find_by
